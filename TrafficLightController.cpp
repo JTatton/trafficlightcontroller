fabcbe724f2e0e5e2e9d56677fb0888154fbcfe2
JTatton <josh.tatton@outlook.com>
2018-07-03 20:44:00 +0930

    added turn on off and flip methods

--- TrafficLightController.cpp
+++ TrafficLightController.cpp
@@ -11,14 +11,39 @@ void Lamp::setpinNumber(int pinNumber){
   _pinNumber = pinNumber;
 }
 
+void Lamp::turnOn(){
+  if (_status == 0){
+    digitalWrite(_pinNumber, LOW);      // Lamp On
+    _status = 1;
+  }
+}
+
+void Lamp::turnOff(){
+  if (_status == 1){
+    digitalWrite(_pinNumber, HIGH);
+    _status == 0;
+  }
+}
+
 void Lamp::turnOn(float seconds){
   digitalWrite(_pinNumber, LOW);       // Lamp ON
   _status = 1;
-  delay(seconds*1000);                // Delay
+  delay(seconds*1000);                 // Delay
   digitalWrite(_pinNumber, HIGH);      // Lamp OFF
   _status = 0;
 }
 
 bool Lamp::getStatus(){
   return _status;
+}
+
+void Lanp::flip(){
+  if (_status == 0){
+    digitalWrite(_pinNumber, LOW);
+    _status == 1;
+  }
+  else{
+    digitalWrite(_pinNumber, HIGH);
+    _status == 0;
+  }
 }
--- TrafficLightController.h
+++ TrafficLightController.h
@@ -12,8 +12,11 @@ class Lamp{
  public:
   Lamp(int pinNumber);              // Constructor Setup Pin Number
   void setpinNumber(int pinNumber); // use if the pinNumber needs to change
+  void turnOn();                    // Turn on Indefnitely
+  void turnOff();                   // Turn off Indefinitely
   void turnOn(float seconds);       // Turn On Lamp for 'seconds' seconds
   bool getStatus();                 // Returns 1 for Lamp is on or 0 if off
+  void flip();                      // If Lamp is Off, Turn it On & Vice Versa
 private:
   int _pinNumber;                   // Stores the pinNumber
   bool _status;                     // Stores whether the light is on 1 or off 0
