93db6e10324a7780125a5411950f043e1270d549
JTatton <josh.tatton@outlook.com>
2018-07-03 20:37:44 +0930

    added status variable and getter

--- TrafficLightController.cpp
+++ TrafficLightController.cpp
@@ -1,17 +1,24 @@
 #include "TrafficLightController.h"
 
 Lamp::Lamp(int pinNumber){
-  Lamp::pinNumber = pinNumber;
-  pinMode(pinNumber, OUTPUT);         // Set pin to output
-  digitalWrite(pinNumber, HIGH);      // Make sure lamp is off
+  _pinNumber = pinNumber;
+  pinMode(_pinNumber, OUTPUT);         // Set pin to output
+  digitalWrite(_pinNumber, HIGH);      // Make sure lamp is off
+  _status = 0;
 }
 
-void Lamp::void setpinNumber(int pinNumber){
-  Lamp::pinNumber = pinNumber;
+void Lamp::setpinNumber(int pinNumber){
+  _pinNumber = pinNumber;
 }
 
 void Lamp::turnOn(float seconds){
-  digitalWrite(pinNumber, LOW);       // Lamp ON
+  digitalWrite(_pinNumber, LOW);       // Lamp ON
+  _status = 1;
   delay(seconds*1000);                // Delay
-  digitalWrite(pinNumber, HIGH);      // Lamp OFF
+  digitalWrite(_pinNumber, HIGH);      // Lamp OFF
+  _status = 0;
+}
+
+bool Lamp::getStatus(){
+  return _status;
 }
--- TrafficLightController.h
+++ TrafficLightController.h
@@ -13,8 +13,10 @@ class Lamp{
   Lamp(int pinNumber);              // Constructor Setup Pin Number
   void setpinNumber(int pinNumber); // use if the pinNumber needs to change
   void turnOn(float seconds);       // Turn On Lamp for 'seconds' seconds
+  bool getStatus();                 // Returns 1 for Lamp is on or 0 if off
 private:
-  int pinNumber;                    // Stores the pinNumber
+  int _pinNumber;                   // Stores the pinNumber
+  bool _status;                     // Stores whether the light is on 1 or off 0
 };
 
 #endif //LAMP_H
