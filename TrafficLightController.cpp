5b014328b0f1e1cd2eaf422c8bd4d2171a01d7ae
Josh Tatton <josh.tatton@outlook.com>
2018-07-11 14:29:42 +0930

    implemented all LED functions

--- TrafficLightController.cpp
+++ TrafficLightController.cpp
@@ -76,3 +76,79 @@ LEDLamp::LEDLamp(int pinNumber, int numLEDs){
 
   FastLED.addLeds<WS2811, _pinNumber, GRB>(_leds, _numLEDs);
 }
+
+void LEDLamp::turnOn(){
+  if (_status == 0){
+    _status = 1;
+    for (int i = 0; i < _numLEDs; i++){   //Set all LEDs in Ring to White
+      _leds[i] = CRGB::White;
+    }
+    FastLED.show();
+  }
+}
+
+void LEDLamp::turnOff(){
+  if (_status == 1){
+    _status = 0;
+    for (int i = 0; i < _numLEDs; i++)  //Set all LEDs in Ring to Black
+    {
+      _leds[i] = CRGB::Black;
+    }
+    FastLED.show();
+  }
+}
+
+void LEDLamp::turnOn(float seconds){
+  _status = 1;
+  for (int i = 0; i < _numLEDs; i++){   //Set all LEDs in Ring to White
+    _leds[i] = CRGB::White;
+  }
+  FastLED.show();
+
+  delay(seconds * 1000);
+
+  _status = 0;
+  for (int i = 0; i < _numLEDs; i++){  //Set all LEDs in Ring to Black
+    _leds[i] = CRGB::Black;
+  }
+  FastLED.show();
+}
+
+void LEDLamp::flip(){
+  if (_status == 1){
+    _status = 0;
+    for (int i = 0; i < _numLEDs; i++)  //Set all LEDs in Ring to Black
+    {
+      _leds[i] = CRGB::Black;
+    }
+    FastLED.show();
+  }
+  else{
+    _status = 1;
+    for (int i = 0; i < _numLEDs; i++){   //Set all LEDs in Ring to White
+      _leds[i] = CRGB::White;
+    }
+    FastLED.show();
+  }
+}
+
+void LEDLamp::flash(float timeOn, float timeOff, int numRepeats){
+  for (int j = 0; j < numRepeats; j++)
+  {
+    for (int i = 0; i < _numLEDs; ++i)
+    {
+      _leds[i] = CRGB::White;
+    }
+    _status = 1;
+    FastLED.show();
+    delay(timeOn*1000);
+
+    for (int i = 0; i < _numLEDs; ++i)
+    {
+      _leds[i] = CRGB::Black;
+    }
+    _status = 0;
+    FastLED.show();
+    delay(timeOff*1000);
+  }
+}
