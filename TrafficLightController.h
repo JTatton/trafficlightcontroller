39cb5a996276d8462a9952696eab1b35a5cf1fe2
JTatton <josh.tatton@outlook.com>
2018-07-19 11:47:20 +0930

    readability of commenting

--- TrafficLightController.h
+++ TrafficLightController.h
@@ -14,26 +14,26 @@ class Lamp{
  public:
   Lamp(uint8_t pinNumber);               // Constructor Setup Pin Number
   void setPinNumber(uint8_t pinNumber);  // use if the pinNumber needs to change
-  virtual void turnOn();             // Turn on Indefinitely
-  virtual void turnOff();            // Turn off Indefinitely
-  virtual void turnOn(float seconds);// Turn On Lamp for 'seconds' seconds
-  bool getStatus();                  // Returns 1 for Lamp is on or 0 if off
-  virtual void flip();               // If Lamp is Off, Turn it On & Vice Versa
+  virtual void turnOn();                 // Turn on Indefinitely
+  virtual void turnOff();                // Turn off Indefinitely
+  virtual void turnOn(float seconds);    // Turn On Lamp for 'seconds' seconds
+  bool getStatus();                      // Returns 1 for Lamp is on or 0 if off
+  virtual void flip();                   // If Lamp is Off, Turn it On & Vice Versa
   virtual void flash(float timeOn, float timeOff, uint8_t numRepeats); // Flash light
 
 protected:
-  uint8_t _pinNumber;                   // Stores the pinNumber
-  bool _status;                     // Stores whether the light is on 1 or off 0
+  uint8_t _pinNumber;                    // Stores the pinNumber
+  bool _status;                          // Stores whether the light is on 1 or off 0
 
 };
 
 class LEDLamp : public Lamp {
   public:
-    LEDLamp(CRGB* leds, uint8_t numLEDs);  // Constructor Setup Pin Number and Num LEDS
-    virtual void turnOn();             // Turn on Indefinitely
-    virtual void turnOff();            // Turn off Indefinitely
-    virtual void turnOn(float seconds);// Turn On Lamp for 'seconds' seconds
-    virtual void flip();               // If Lamp is Off, Turn it On & Vice Versa
+    LEDLamp(CRGB* leds, uint8_t numLEDs);// Constructor Setup Pin Number and Num LEDS
+    virtual void turnOn();               // Turn on Indefinitely
+    virtual void turnOff();              // Turn off Indefinitely
+    virtual void turnOn(float seconds);  // Turn On Lamp for 'seconds' seconds
+    virtual void flip();                 // If Lamp is Off, Turn it On & Vice Versa
     virtual void flash(float timeOn, float timeOff, uint8_t numRepeats); // Flash light
 
   private:
